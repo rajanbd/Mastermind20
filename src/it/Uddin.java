dc271d7736c9fed506439a99f9c9fcc6f4c0b0c5
rajanbd <rajan.bd.usa@gmail.com>
2022-04-12 11:36:44 -0700

    second commit

---
+++ src/it/Uddin.java
@@ -0,0 +1,20 @@
+package it;
+
+public class Uddin {
+
+	public static void main(String[] args) {
+		
+	}
+         int a=9;
+         int b=8;
+         int m=a+b;
+        private void Syco() {
+			// TODO Auto-generated method stub
+
+		}
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+}
